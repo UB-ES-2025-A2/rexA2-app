e2a1aea69d06d7b291de79027c846f8a07276f9c
AlexRamirez5 <aramirto20@alumnes.ub.edu>
2025-11-11 17:39:45 +0100

    Solución del problema de register Falta:     - Arreglar en el frontend el "refresh al editar" entre usuarios

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from ..db.models import user as user_crud
 from ..db.schemas.user import UserCreate, UserPublic
 
+
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post('', response_model=UserPublic, status_code=201)
@@ -21,19 +22,19 @@ async def register_user(payload: UserCreate):
         email=payload.email,
         password=payload.password,
         username=payload.username,
-        name=payload.name,
-        phone=payload.phone,
-        preferred_units=payload.preferred_units,
-        avatar_url=payload.avatar_url,
+        #name=payload.name,
+        #phone=payload.phone,
+        #preferred_units=payload.preferred_units,
+        #avatar_url=payload.avatar_url,
     )
 
     return {
         "id": str(user["_id"]),
         "email": user["email"],
         "username": user.get("username"),
-        "name": user.get("name"),
-        "phone": user.get("phone"),
-        "preferred_units": user.get("preferred_units") or "km",
-        "avatar_url": user.get("avatar_url"),
+        #"name": user.get("name"),
+        #"phone": user.get("phone"),
+        #"preferred_units": user.get("preferred_units") or "km",
+        #"avatar_url": user.get("avatar_url"),
         "is_active": user["is_active"],
     }
